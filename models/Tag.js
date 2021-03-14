06a17f2ddff9b4cfb566304ca94d6e7c503f1e50
Chelle77322 <serenynadarra@gmail.com>
2021-03-14 15:46:15 +1030

    Coding the tag model extend

--- models/Tag.js
+++ models/Tag.js
@@ -1,3 +1,4 @@
+//Setting up the class constructor Model and extending it to be used for TAG
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -6,7 +7,14 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // define columns of the table
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    tag_name: { type: DataTypes.STRING}
   },
   {
     sequelize,
