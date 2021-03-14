558bcebda71f7315fe2b4c013e78048e9ad87d91
Chelle77322 <serenynadarra@gmail.com>
2021-03-14 13:20:18 +1030

    Begin configuration of tag-routes file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', (request, result) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product
+    }
+  })
+  .then(tagData => result.json(tagData))
+  .catch(error => {
+    console.log(error);
+    result.status(500).json(error);
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (request, result) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', (request, result) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', (request, result) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (request, result) => {
   // delete on tag by its `id` value
 });
 
