039f74ca309f213d027c35371d041109795fb96e
Chelle77322 <serenynadarra@gmail.com>
2021-03-14 21:26:21 +1030

    Additional changes to the config setup

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 const Sequelize = require('sequelize');
-require ('dotenv').config();
+require ('dotenv').config({path: __dirname +`/../env`});
 require ('config-json');
 
 let sequelize;
 if (process.env.JAWSDB_URL){
   sequelize =  new Sequelize(process.env.JAWSDB_URL);
-}else{
+} else {
   sequelize = new Sequelize(
-    process.env.DB_HOST,
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD, 
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
+      host:'localhost',
+      dialect:'mysql',
+      port: 4002,
       dialectOptions: {
         decimalNumbers: true,
       },
@@ -22,6 +21,7 @@ if (process.env.JAWSDB_URL){
 }
 
   console.log(sequelize);
+ 
   
 
 module.exports = sequelize;
