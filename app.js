aaeb5d47e102ab3e7044f09823be2b037c4242fe
Chelle77322 <serenynadarra@gmail.com>
2021-03-15 13:29:04 +1030

    Changing the port used to get rid of EADDRINUSE error

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const sequelize =require("./config/connection.js");
-const app = express();
-const mysql = require('mysql2');
-console.log(sequelize);
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+//const express = require('express');
+//const sequelize =require("./config/connection.js");
+//const app = express();
+//const mysql = require('mysql2');
+//console.log(sequelize);
+//app.use(express.json());
+//app.use(express.urlencoded({extended: true}));
 
-sequelize.authentication ().then (()=> {
-  console.log('Connection established successfully.');
-  console.log(sequelize);
+//sequelize.authentication ().then (()=> {
+  //console.log('Connection established successfully.');
+  //console.log(sequelize);
 
-}).catch (error =>{
-  console.error("Unable to connect to the database:", error);
-}).finally(()=> {
-  sequelize.close();
+//}).catch (error =>{
+  //console.error("Unable to connect to the database:", error);
+//}).finally(()=> {
+  //sequelize.close();
 
-});
+//});
