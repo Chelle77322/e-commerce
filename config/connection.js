4d0b5f50453b5daf5ba71b65e3de31e64686df96
Chelle77322 <serenynadarra@gmail.com>
2021-03-15 11:40:52 +1030

    Trying to work out why sequelize connection isn't allowing admin user

--- config/connection.js
+++ config/connection.js
@@ -10,18 +10,14 @@ if (process.env.JAWSDB_URL){
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD, 
-    {
+  {
       host:'localhost',
       dialect:'mysql',
-      port: 4002,
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
     });
 }
-
-  console.log(sequelize);
- 
-  
-
+console.log(sequelize);
 module.exports = sequelize;
